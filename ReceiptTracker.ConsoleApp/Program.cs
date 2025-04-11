a0c6df59d6a314a2be621a4f58e9825a9706a8a8
jholl1886 <jacobholleman1886@gmail.com>
2025-04-10 23:13:55 -0400

    Didnt mean to be working on master here

--- ReceiptTracker.ConsoleApp/Program.cs
+++ ReceiptTracker.ConsoleApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptTracker.ConsoleApp.Models;
+using ReceiptTracker.ConsoleApp.Services;
+
+ReceiptServiceProxy receiptService = new ReceiptServiceProxy();
+
+int check = 0;
+while (check == 0)
+{
+    Console.WriteLine("Select you option");
+    Console.WriteLine("1: Add a recipe");
+    Console.WriteLine("2: See all recipes");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    if (choice == 1)
+    {
+        Console.WriteLine("Enter Receipt date");
+
+        Console.WriteLine("Enter Receipt amount");
+
+        Console.WriteLine("Enter Receipt Description");
+    }
+    else if (choice == 2)
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("See you later");
+        check = 1;
+    }
+
+}
---
+++ ReceiptTracker.ConsoleApp/Services/ReceiptServiceProxy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptTracker.ConsoleApp.Models;
+
+namespace ReceiptTracker.ConsoleApp.Services
+{
+    public class ReceiptServiceProxy
+    {
+        private List<Receipt> Receipts;
+
+        //Contructor to initialize the list
+        public ReceiptServiceProxy() 
+        {
+            Receipts = new List<Receipt>();
+        }
+
+        public void AddReceipt(Receipt receipt)
+        {
+            
+            Receipts.Add(receipt);
+        }
+
+        public List<Receipt> GetAllReceipts()
+        {
+            return Receipts;
+        }
+    }
+}
