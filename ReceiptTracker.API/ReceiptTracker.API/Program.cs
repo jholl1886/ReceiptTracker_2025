692d7c1a7dcb513b9c0d8cc7b6d2c63868a57aad
jholl1886 <jacobholleman1886@gmail.com>
2025-04-12 23:17:23 -0400

    Had to update Program.cs in API to create the DB, also added ability to add onto later to add files

--- ReceiptTracker.API/ReceiptTracker.API/Program.cs
+++ ReceiptTracker.API/ReceiptTracker.API/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using ReceiptTracker.API.Data;
+//using ReceiptTracker.API.Services; will beed to add later
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); //SQLite should be configured now
+
+//builder.Services.AddScoped<IReceiptService, ReceiptService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient", policy =>
+    {
+        policy.WithOrigins("https://localhost:7265", "http://localhost:5146")
+              .AllowAnyMethod() //probably only using post but not sure yet
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,7 +34,28 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 
+    
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.EnsureCreated(); //if the db wasnt made yet or I deleted it, will be here now
+    }
+
 }
+
+var uploadsDir = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+if (!Directory.Exists(uploadsDir))
+{
+    Directory.CreateDirectory(uploadsDir); //I may not need this cause I made it myself not sure
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(uploadsDir), //will allow .pdfs to be uploaded along with .pngs
+    RequestPath = "/uploads"
+});
+
+app.UseCors("AllowBlazorClient");
 
 app.UseHttpsRedirection();
 
