689578da61f0482847a834e55edc1ea580c35b24
jholl1886 <jacobholleman1886@gmail.com>
2025-04-12 23:55:11 -0400

    added the interface first

---
+++ ReceiptTracker.API/ReceiptTracker.API/Services/IReceiptService.cs
@@ -0,0 +1,14 @@
+﻿using ReceiptTracker.Shared.Models;
+
+namespace ReceiptTracker.API.Services
+{
+    public interface IReceiptService
+    {
+        //wanted to use interfaces this time cause watched a youtube video awhile ago that said " you want to write code that calls the interface not the actual class" loosely coupled
+        Task<IEnumerable<Receipt>> GetAllReceiptsAsync();
+        Task<Receipt?> GetReceiptByIdAsync(string id);
+        Task<Receipt> AddReceiptAsync(Receipt receipt);
+        Task<bool> UpdateReceiptAsync(Receipt receipt);
+        Task<bool> DeleteReceiptAsync(string id);
+    }
+}
