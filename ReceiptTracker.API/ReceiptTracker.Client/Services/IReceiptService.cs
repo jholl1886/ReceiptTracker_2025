4ae03a59572b7149258eb43b1cbae12741e416f6
jholl1886 <jacobholleman1886@gmail.com>
2025-04-13 00:49:56 -0400

    Added Viewmodel folder also changed page folder to be called views and added the same interface file again

---
+++ ReceiptTracker.API/ReceiptTracker.Client/Services/IReceiptService.cs
@@ -0,0 +1,14 @@
+﻿using ReceiptTracker.Shared.DTO;
+
+namespace ReceiptTracker.Client.Services
+{
+    public interface IReceiptService
+    {
+        Task<IEnumerable<ReceiptDTO>> GetAllReceiptsAsync();
+        Task<ReceiptDTO?> GetReceiptByIdAsync(string id);
+        Task<ReceiptDTO> AddReceiptAsync(ReceiptDTO receipt, byte[]? fileData = null, string? fileContentType = null);
+        Task<bool> UpdateReceiptAsync(ReceiptDTO receipt);
+        Task<bool> DeleteReceiptAsync(string id);
+    }
+}
+
